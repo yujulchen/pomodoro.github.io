22ada409307253ffe10c9237f49b0eadd07b8386
YU <gilbertjulchen0118@gmail.com>
2021-03-19 04:01:05 +0800

    可以新增待辦事項了，修改跟刪除待處理

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -10,12 +10,16 @@ import Change from "./svg/Change";
 import Mute from "./svg/Mute";
 
 function Clock() {
-  const [work, setWork] = useState(5);
-  const [rest, setRest] = useState(8);
+  // 工作計時(25分鐘)
+  const [work, setWork] = useState(25 * 60);
+  // 休息計時(5分鐘)
+  const [rest, setRest] = useState(5 * 60);
+  // 時間顯示
   const [workState, setWorkState] = useState(work);
+  // 開始/暫停
   const [play, setPlay] = useState(false);
+  // 音樂播放(預設為停止)
   const [autoplay, setAutoplay] = useState(0);
-
   // 影片播放設定
   const opts = {
     width: "560",
@@ -27,29 +31,37 @@ function Clock() {
 
   // 計時器
   useEffect(() => {
+    // setInterval 每秒執行一次
     const int = setInterval(() => {
-      // console.log(`${Date.now()} - paused: ${play}`);
+      // 如果點下開始計時鈕且計時器數字不等於0
       if (play === true && workState !== 0) {
+        // 先不撥放音樂
         setAutoplay(0);
+        // 計時器的數字每秒-1
         setWorkState(workState - 1);
       }
+      // 如果計時器數字等於0
       if (workState === 0) {
+        // 切換為預設值
         setPlay(false);
+        // 將計時器的數字設成工作計時
         setWorkState(work);
+        // 時間到撥放音樂
         setAutoplay(1);
       }
     }, 1000);
     return () => {
+      // 清除setInterval
       clearInterval(int);
     };
   }, [play, workState]);
 
-  // 開始計時/暫停計時
+  // 開始計時/暫停計時icon切換
   function changePlay() {
     play === false ? setPlay(true) : setPlay(false);
   }
 
-  // 恢復預設
+  // 恢復預設計時
   function stopPlay() {
     setPlay(false);
     setWorkState(work);
@@ -71,11 +83,12 @@ function Clock() {
 
   return (
     <>
-      <div className="timeBox">{`${Math.floor(workState / 60)}:${(
-        "00" +
-        (workState % 60)
-      ).slice(-2)}`}</div>
+      {/* 將秒數轉為 分：秒 */}
+      <div className="timeBox">
+        {`${Math.floor(workState / 60)}:${("00" + (workState % 60)).slice(-2)}`}
+      </div>
       <div className="iconBox">
+        {/* 恢復為預設計時 */}
         <div
           className="stop"
           onClick={() => {
@@ -84,6 +97,7 @@ function Clock() {
         >
           <Stop />
         </div>
+        {/* 開始/暫停計時 */}
         <div
           className="play"
           onClick={() => {
@@ -97,6 +111,7 @@ function Clock() {
             <Pause />
           </div>
         </div>
+        {/* 切換工作計時/休息計時 */}
         <div
           className="change"
           onClick={() => {
@@ -107,12 +122,14 @@ function Clock() {
         </div>
       </div>
       <div className="musicBox">
+        {/* 將音樂停止 */}
         <div
           className="music"
           onClick={() => {
             changeVoice();
           }}
         >
+          {/* 內嵌yt撥放器 */}
           <div style={{ display: "none" }}>
             <YouTube videoId="xCxP5_wpGiQ" opts={opts}></YouTube>
           </div>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,11 +1,77 @@
-import { usestate, useeffect } from "react";
+import { useState, useEffect } from "react";
 import Plus from "./svg/Plus";
+import Edit from "./svg/Edit";
+import Delete from "./svg/Delete";
 
 function TodoList() {
+  const [todo, setTodo] = useState("");
+
+  // 每個事件的物件值
+  // {id:number, text:string, completed:bool}
+  const [todoLists, setTodoLists] = useState([
+    { id: 2, text: "Todo2", completed: false },
+    { id: 1, text: "Todo1", completed: true },
+  ]);
+
+  // 新增
+  const handleAddNew = (e) => {
+    // 判斷是否按下Enter鍵，並且非是空白未填的情況(用trim去掉頭尾空白)
+    if (e.key === "Enter" && e.target.value.trim()) {
+      // 增加新的待辦事項
+      const newTodoItem = {
+        id: +new Date(),
+        text: e.target.value,
+        completed: false,
+      };
+
+      // 在待辦事項列表最前面加入新的待辦事項
+      const newTodos = [newTodoItem, ...todoLists];
+
+      // 更新為新的待辦事項清單
+      setTodoLists(newTodos);
+
+      // 清空輸入文字
+      setTodo("");
+    }
+  };
+
+  // 切換是否已完成
+  const handleCompleted = (id) => {
+    // 先拷貝一個新的陣列
+    const newTodo = [...todoLists];
+
+    // 利用id值找對應的todo item的索引值
+    const index = newTodo.findIndex((item) => item.id === id);
+
+    // 如果有找到就不會是-1
+    if (index !== -1) {
+      // 切換布林值
+      newTodo[index].completed = !newTodo[index].completed;
+    }
+
+    // 設定回原本的todo
+    setTodoLists(newTodo);
+
+    // 清空輸入文字
+    setTodo("");
+  };
+
   return (
     <>
       <div className="addNewTodo">
-        <input type="text" name="add" id="add" placeholder="add todo" />
+        <label htmlFor="add"></label>
+        <input
+          type="text"
+          id="add"
+          placeholder="add todo"
+          value={todo}
+          onChange={(e) => {
+            setTodo(e.target.value);
+          }}
+          onKeyPress={(e) => {
+            handleAddNew(e);
+          }}
+        />
         <div type="submit" className="plus">
           <div className="plusIn">
             <Plus />
@@ -14,11 +80,36 @@ function TodoList() {
       </div>
       <div className="list">
         <ul>
-          <li>Todo1</li>
-          <li>Todo2</li>
-          <li>Todo3</li>
-          <li>Todo4</li>
-          <li>Todo5</li>
+          {/* 將陣列map出來，並加上key值 */}
+          {todoLists.map((item, index) => {
+            /* 用completed判斷是否要加刪除線 */
+            return (
+              <div className="listBox">
+                <label htmlFor={item.id}>
+                  <li key={item.id}>
+                    <input
+                      id={item.id}
+                      type="checkbox"
+                      className="check"
+                      checked={item.completed}
+                      onChange={() => {
+                        handleCompleted(item.id);
+                      }}
+                    />
+                    {item.completed ? <del>{item.text}</del> : item.text}
+                  </li>
+                </label>
+                <div className="liIconBox">
+                  <div className="edit">
+                    <Edit />
+                  </div>
+                  <div className="delete">
+                    <Delete />
+                  </div>
+                </div>
+              </div>
+            );
+          })}
         </ul>
       </div>
     </>
