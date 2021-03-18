1b98367cb5c3647249bf3c2de21e6e19ee7432dd
YU <gilbertjulchen0118@gmail.com>
2021-03-18 21:26:30 +0800

    計時器完成

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import moment from "moment";
+import YouTube from "react-youtube";
 
 // svg components
 import ClockCircle from "./svg/ClockCircle";
@@ -7,24 +7,56 @@ import Stop from "./svg/Stop";
 import Play from "./svg/Play";
 import Pause from "./svg/Pause";
 import Change from "./svg/Change";
-import Music from "./svg/Music";
+import Mute from "./svg/Mute";
 
 function Clock() {
-  const [work, setWork] = useState(1500000);
-  const [rest, setRest] = useState(300000);
+  const [work, setWork] = useState(5);
+  const [rest, setRest] = useState(8);
   const [workState, setWorkState] = useState(work);
   const [play, setPlay] = useState(false);
+  const [autoplay, setAutoplay] = useState(0);
 
-  function ChangePlay() {
-    play === true ? setPlay(false) : setPlay(true);
+  // 影片播放設定
+  const opts = {
+    width: "560",
+    height: "315",
+    playerVars: {
+      autoplay: autoplay,
+    },
+  };
+
+  // 計時器
+  useEffect(() => {
+    const int = setInterval(() => {
+      // console.log(`${Date.now()} - paused: ${play}`);
+      if (play === true && workState !== 0) {
+        setAutoplay(0);
+        setWorkState(workState - 1);
+      }
+      if (workState === 0) {
+        setPlay(false);
+        setWorkState(work);
+        setAutoplay(1);
+      }
+    }, 1000);
+    return () => {
+      clearInterval(int);
+    };
+  }, [play, workState]);
+
+  // 開始計時/暫停計時
+  function changePlay() {
+    play === false ? setPlay(true) : setPlay(false);
   }
 
+  // 恢復預設
   function stopPlay() {
+    setPlay(false);
     setWorkState(work);
   }
 
-  function ChangeState() {
-    console.log("change");
+  // 切換休息/工作計時
+  function changeState() {
     if (workState === work) {
       setWorkState(rest);
     } else {
@@ -32,13 +64,17 @@ function Clock() {
     }
   }
 
-  // TODO: 計時器製作中
-  const nowTime = moment().format("YYYY-MM-DD HH:mm:ss");
-  console.log("nowTime", nowTime);
+  // 把音樂停掉
+  function changeVoice() {
+    setAutoplay(0);
+  }
 
   return (
     <>
-      <div className="timeBox">{workState}</div>
+      <div className="timeBox">{`${Math.floor(workState / 60)}:${(
+        "00" +
+        (workState % 60)
+      ).slice(-2)}`}</div>
       <div className="iconBox">
         <div
           className="stop"
@@ -51,28 +87,36 @@ function Clock() {
         <div
           className="play"
           onClick={() => {
-            ChangePlay();
+            changePlay();
           }}
         >
-          <div style={{ display: play === true ? "none" : "block" }}>
+          <div style={{ display: play === false ? "block" : "none" }}>
             <Play />
           </div>
-          <div style={{ display: play === true ? "block" : "none" }}>
+          <div style={{ display: play === false ? "none" : "block" }}>
             <Pause />
           </div>
         </div>
         <div
           className="change"
           onClick={() => {
-            ChangeState();
+            changeState();
           }}
         >
           <Change />
         </div>
       </div>
       <div className="musicBox">
-        <div className="music">
-          <Music />
+        <div
+          className="music"
+          onClick={() => {
+            changeVoice();
+          }}
+        >
+          <div style={{ display: "none" }}>
+            <YouTube videoId="xCxP5_wpGiQ" opts={opts}></YouTube>
+          </div>
+          <Mute />
         </div>
       </div>
       <div className="circle">
